8bd966d8f886ca5b1ee1f34ce38ccdabe6222d50
Toni Perkusic <tperku00@fesb.hr>
2020-10-20 19:30:53 +0200

    adding navigation header

---
+++ src/components/NavigationHeader/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import styles from "./style.module.css"
+
+
+
+const NavigationHeader = () => (
+
+<main>
+    <section className={styles.navigationHeader}>
+    </section>
+
+</main>
+
+)
+
+export default NavigationHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 import ContactBar from "../components/contactBar/index.js"
+import NavigationHeader from "../components/NavigationHeader/index.js"
 
 const IndexPage = () => (
   <main>
     <ContactBar />
+    <NavigationHeader/>
   </main>
 )
 
