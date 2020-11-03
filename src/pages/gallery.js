c44bf11aeb4fd5d78ef52648b0ed6e0e32244bbd
Toni Perkusic <tperku00@fesb.hr>
2020-11-03 17:34:31 +0100

    adding photo gallery

---
+++ src/pages/gallery.js
@@ -0,0 +1,11 @@
+import React from "react"
+import HeaderFooterLayout from "../layouts/headerFooter.js"
+import SeparatorBar from "../modules/SeparatorBar/index.js"
+
+const GalleryPage = () => (
+  <HeaderFooterLayout activeTab="Photo Gallery">
+    <SeparatorBar text="Photo Gallery" />
+  </HeaderFooterLayout>
+)
+
+export default GalleryPage
