8d5eac5ad1fd0832e75d35f874e0a5ca2b34ff13
Toni Perkusic <tperku00@fesb.hr>
2020-11-03 15:24:12 +0100

    adding combobox picker

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,14 +1,19 @@
 import React from "react"
 import NavigationBar from "../NavigationBar/index.js"
-import Combobox from "../Combobox/index.js"
+import ReactFlagsSelect from "react-flags-select"
+import "react-flags-select/css/react-flags-select.css"
 import styles from "./style.module.css"
 
 const Navigation = ({ activeTab }) => (
-    <section className = {styles.navigation}>
-        <NavigationBar activeTab = {activeTab}/>
-        <Combobox/>
-    </section>
+  <section className={styles.navigation}>
+    <NavigationBar activeTab={activeTab} />
+    <ReactFlagsSelect
+      countries={["GB", "IT", "DE"]}
+      className={styles.combobox}
+      placeholder="ENGLISH"
+      customLabels={{ GB: "English", DE: "Deutsche", IT: "Italiano" }}
+    />
+  </section>
 )
-
 
 export default Navigation
