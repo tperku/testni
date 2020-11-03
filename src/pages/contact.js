ce5a56b5c798e71c44bd03399af51f4587e602c2
Toni Perkusic <tperku00@fesb.hr>
2020-11-03 16:06:14 +0100

    dodavanje sadrzaja contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,13 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter.js"
+import ContactSeparator from "../modules/ContactSeparator/index.js"
+import ContactForm from "../modules/ContactForm/index.js"
 
 const ContactPage = () => (
-  <HeaderFooterLayout activeTab="Contact"></HeaderFooterLayout>
+  <HeaderFooterLayout activeTab="Contact">
+    <ContactSeparator />
+    <ContactForm />
+  </HeaderFooterLayout>
 )
 
 export default ContactPage
